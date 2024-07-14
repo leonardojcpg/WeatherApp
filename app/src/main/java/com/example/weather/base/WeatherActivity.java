f6ec07231403b7e554c766467429d5155005ba37
leonardoGomes <leonardojpinheirogomes@gmail.com>
2024-07-14 00:54:56 -0300

    feat: Creating Project Weather App

---
+++ app/src/main/java/com/example/weather/base/WeatherActivity.java
@@ -0,0 +1,9 @@
+package com.example.weather.base;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeatherActivity extends AppCompatActivity {
+    /**
+     * Classe base que usarei futuramente para estruturar o projeto da maneira correta
+     */
+}
